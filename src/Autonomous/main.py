9ac2fd98b6fe2e2326e9d3f0d633159ae802b529
Richard <richard.perdomo19@gmail.com>
2024-03-27 20:43:28 +0000

    Filled out the heading code, not fully working yet with which direction to turn

--- src/Autonomous/main.py
+++ src/Autonomous/main.py
@@ -1,5 +1,7 @@
+import math
+import numpy as np
 import Navigation as nv
-import AruCoLib as AL
+#import AruCoLib as AL
 
 def getGPSCoordinates(GPSLoc):
     '''Looks for the JSON file that contains the necessary GPS coordinates and returns them.
@@ -8,13 +10,47 @@ def getGPSCoordinates(GPSLoc):
     '''
     pass
 
-def calculateHeading(visual=False, GLL=None, pixLoc=None):
+def calculateHeading(visual=False, GLL: list[list[float]]=None, pixLoc=None):
     '''Calculates which direction to go based on inputs.
-       Inputs: visual (true/false), GLL (GPS lat and long), pixLoc (pixel location in image)
+       Inputs: visual (true/false), GLL (current GPS lat and long, dest GPS lat and long), pixLoc (pixel location in image)
        Output: will figure that out when i start ROS stuff
     '''
-    pass
+    currentPos = GLL[0]
+    destinationPos = GLL[1]
 
+    dir = headingDir(currentPos, destinationPos)
+
+    print("CurrentPos: {}, destination: {}".format(currentPos, destinationPos))
+
+    currentAng = 0
+    print("CurrentAng: {}".format(currentAng))
+
+    currentDir = [math.cos(currentAng), math.sin(currentAng)]
+    currentDir = [p/coordinateNorm(currentPos) for p in currentDir]
+    print("Current direction: {}".format(currentDir))
+
+    targetDir = [destinationPos[0]-currentPos[0], destinationPos[1]-currentPos[1]]
+    #targetDir = [destinationPos[0], destinationPos[1]]
+    targetDir = [p/coordinateNorm(destinationPos) for p in targetDir]
+    print("Target point direction: {}".format(targetDir))
+
+    delta = math.atan(np.linalg.norm(np.cross(targetDir, currentDir))/np.dot(targetDir, currentDir))
+    print("delta: {}".format(dir*math.degrees(delta)))
+    
+    return delta
+
+def coordinateNorm(point: list[float]):
+    return math.sqrt((point[0]**2)+(point[1]**2))
+
+def headingDir(GLLc: list[float]=None, GLLt: list[float]=None):
+    if GLLc[1] > GLLt[1]:
+        return -1
+    elif GLLc[1] < GLLt[1]:
+        return 1
+    else:
+        return 1
+    
+'''
 def main():
     GPSSentence1 = ""
     GPSSentence2 = ""
@@ -28,7 +64,8 @@ def main():
     for destination in GPSCoor:
         GPSSentence1 = GPS1.read() # read until end character
         GPSSentence2 = GPS2.read() # read until end character
-    pass
+    pass'''
 
 if __name__ == "__main__":
-    main()
+    #main()
+    calculateHeading(GLL=[[10.0, 6.0], [4.0, 5.0]])
